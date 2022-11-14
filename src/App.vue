d136b87fe55b7097eaddc7094fc79a8bcad1537f
김동률 <lancelot1672@naver.com>
2022-11-14 22:04:01 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <header-nav></header-nav>
+    <v-main class="grey lighten-3">
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HeaderNav from "@/components/HeaderNav.vue";
+export default {
+  name: 'App',
+  components:{
+    HeaderNav
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
