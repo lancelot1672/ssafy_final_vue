84300458537afc02df5189ac5f17c4bef93cf2ca
lancelot1672 <lancelot1672@naver.com>
2022-11-15 14:15:10 +0900

    modify UI

--- src/components/AptSimpleInfo.vue
+++ src/components/AptSimpleInfo.vue
@@ -1,40 +1,37 @@
 <template>
-    <div class="abc">
+  <div class="abc">
     <v-container v-if="house">
-        <v-row>
-            <v-col cols="12" sm="12">
-                <v-sheet min-height="10vh" rounded="lg">
-                    {{house.name}}
-                </v-sheet>
-            </v-col>
-        </v-row>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-sheet min-height="10vh" rounded="lg">
+            {{ house.name }}
+          </v-sheet>
+        </v-col>
+      </v-row>
     </v-container>
-    </div>
+  </div>
 </template>
 <script>
 import { mapState } from "vuex";
-export default{
-    name: "AptSimpleInfo",
-    computed: {
+export default {
+  name: "AptSimpleInfo",
+  computed: {
     ...mapState(["house"]),
     // house() {
     //   return this.$store.state.house;
     // },
   },
-  created(){
+  created() {
     console.log("create");
   },
-    data(){
-        return {
-        }
-    },
-    methods:{
-    }
-    
-}
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 <style scoped>
-    .v-sheet{
+/* .v-sheet{
 
         height: 100px;
         transition: 0.5s;
@@ -42,5 +39,5 @@ export default{
     }
     .v-sheet:hover {
         height: 200px;
-    }
+    } */
 </style>
--- src/components/SelectCondition.vue
+++ src/components/SelectCondition.vue
@@ -2,70 +2,69 @@
   <v-container>
     <v-row>
       <v-col cols="12" sm="12">
-        <v-sheet min-height="10vh" rounded="lg">
-          <v-col cols="12" sm="8" class="select_section">
-            <v-select
-              class="select"
-              label="시도"
-              :items="sidoList"
-              placeholder="select"
-              v-model="sido"
-              @change="sidoChange()"
-            >
-              <option v-for="sido in sidoList" :key="sido" :value="sido">
-                {{ sido }}
-              </option>
-            </v-select>
-            <v-select
-              class="select"
-              label="구군"
-              :items="gugunList"
-              placeholder="select"
-              v-model="gugun"
-              @change="gugunChange()"
-            >
-              <option v-for="gugun in gugunList" :key="gugun" :value="gugun">
-                {{ gugun }}
-              </option>
-            </v-select>
-            <v-select
-              class="select"
-              label="법정동"
-              :items="dongList"
-              placeholder="select"
-              v-model="dong"
-              @change="dongChange()"
-            >
-              <option v-for="dong in dongList" :key="dong" :value="dong">
-                {{ dong }}
-              </option>
-            </v-select>
-            <v-select
-              class="select"
-              label="년"
-              :items="yearList"
-              placeholder="select"
-              v-model="year"
-              @change="yearChange()"
-            >
-              <option v-for="year in yearList" :key="year" :value="year">
-                {{ gugun }}
-              </option>
-            </v-select>
-            <v-select
-              class="select"
-              label="월"
-              :items="monthList"
-              placeholder="select"
-              v-model="month"
-              @change="monthChange()"
-            >
-              <option v-for="month in monthList" :key="month" :value="month">
-                {{ month }}
-              </option>
-            </v-select>
-          </v-col>
-        </v-sheet>
+        <div class="select_section" min-height="10vh" rounded="lg">
+          <v-select
+            class="select"
+            label="시도"
+            :items="sidoList"
+            placeholder="select"
+            v-model="sido"
+            @change="sidoChange()"
+          >
+            <option v-for="sido in sidoList" :key="sido" :value="sido">
+              {{ sido }}
+            </option>
+          </v-select>
+          <v-select
+            class="select"
+            label="구군"
+            :items="gugunList"
+            placeholder="select"
+            v-model="gugun"
+            @change="gugunChange()"
+          >
+            <option v-for="gugun in gugunList" :key="gugun" :value="gugun">
+              {{ gugun }}
+            </option>
+          </v-select>
+          <v-select
+            class="select"
+            label="법정동"
+            :items="dongList"
+            placeholder="select"
+            v-model="dong"
+            @change="dongChange()"
+          >
+            <option v-for="dong in dongList" :key="dong" :value="dong">
+              {{ dong }}
+            </option>
+          </v-select>
+          <v-select
+            class="select"
+            label="년"
+            :items="yearList"
+            placeholder="select"
+            v-model="year"
+            @change="yearChange()"
+          >
+            <option v-for="year in yearList" :key="year" :value="year">
+              {{ gugun }}
+            </option>
+          </v-select>
+          <v-select
+            class="select"
+            label="월"
+            :items="monthList"
+            placeholder="select"
+            v-model="month"
+            @change="monthChange()"
+          >
+            <option v-for="month in monthList" :key="month" :value="month">
+              {{ month }}
+            </option>
+          </v-select>
+          <v-btn class="search_btn">검색</v-btn>
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -114,12 +113,9 @@ export default {
         });
     },
     gugunChange() {
-      fetch(
-        `http://localhost:9999/home/dongName?sidoName=${this.sido}&gugunName=${this.gugun}`,
-        {
-          method: "get",
-        }
-      )
+      fetch(`http://localhost:9999/home/dongName?sidoName=${this.sido}&gugunName=${this.gugun}`, {
+        method: "get",
+      })
         .then((resp) => resp.json())
         .then((data) => {
           for (let i = 0; i < data.length; i++) {
@@ -138,12 +134,9 @@ export default {
       }
     },
     monthChange() {
-      fetch(
-        `http://localhost:9999/home/dongCode?dongName=${this.dong}&sidoName=${this.sido}`,
-        {
-          method: "get",
-        }
-      )
+      fetch(`http://localhost:9999/home/dongCode?dongName=${this.dong}&sidoName=${this.sido}`, {
+        method: "get",
+      })
         .then((resp) => resp.json())
         .then((data) => {
           this.dongCode = data;
@@ -166,9 +159,18 @@ export default {
 <style scoped>
 .select_section {
   display: flex;
+  padding-left: 200px;
+  padding-right: 50px;
+  border: 1px solid #757575;
+  border-radius: 10px;
 }
 .select {
-  margin: 0px 50px;
-  width: 150px;
+  margin: 0px 20px;
+  width: 300px;
+}
+.search_btn {
+  width: 200px;
+  margin-top: 10px;
+  background: none;
 }
 </style>
