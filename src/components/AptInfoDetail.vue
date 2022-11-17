36f62cbf8d2a6606d38e2787ba26a825e76c2a3b
lancelot1672 <lancelot1672@naver.com>
2022-11-17 17:50:38 +0900

    [UPDATE] AptInfoDetail.vue

--- src/components/AptInfoDetail.vue
+++ src/components/AptInfoDetail.vue
@@ -7,58 +7,81 @@
         </div>
         <div class="detailMiddle">
           <div class="detailMiddleLeft">
-            <img
-              src="@/assets/detailbighouse.jpg"
-              width="200px"
-              style="
-                border-radius: 50px;
-                -moz-border-radius: 70px;
-                -khtml-border-radius: 70px;
-                -webkit-border-radius: 70px;
-              "
-            />
-            <br />
-            <strong> {{ aptDetailInfo.area }} ㎡ </strong>
+            <div class="detail-img">
+              <img
+                src="@/assets/detailbighouse.jpg"
+                width="200px"
+                style="
+                  border-radius: 50px;
+                  -moz-border-radius: 70px;
+                  -khtml-border-radius: 70px;
+                  -webkit-border-radius: 70px;
+                "
+              />
+              <br />
+              <strong> {{ aptDetailInfo.area }} ㎡ </strong>
+            </div>
+            <!--  simple info -->
+            <table>
+              <tr>
+                <td>
+                  <v-chip :color="`red lighten-4`" label class="info-item"
+                    ><strong>검색주소</strong></v-chip
+                  >
+                </td>
+                <td>
+                  &nbsp;
+                  <strong
+                    >{{ sido }} {{ aptDetailInfo.dongName }} {{ aptDetailInfo.roadName }}
+                    {{ aptDetailInfo.jibun }}</strong
+                  >
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-chip :color="`red lighten-4`" label class="info-item"
+                    ><strong>거래 날짜</strong></v-chip
+                  >
+                </td>
+                <td>
+                  &nbsp;
+                  <strong>{{ aptDetailInfo.dealYear }}년 {{ aptDetailInfo.dealMonth }}월</strong>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-chip :color="`red lighten-4`" label class="info-item"
+                    ><strong>건축 년도</strong></v-chip
+                  >
+                </td>
+                <td>
+                  &nbsp; <strong>{{ aptDetailInfo.buildYear }}년</strong>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-chip :color="`red lighten-4`" label class="info-item">
+                    <strong>층</strong></v-chip
+                  >
+                </td>
+                <td>
+                  &nbsp; <strong>{{ aptDetailInfo.floor }}층</strong>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-chip :color="`red lighten-4`" label class="info-item">
+                    <strong>거래 금액</strong></v-chip
+                  >
+                </td>
+                <td>
+                  &nbsp; <strong>{{ aptDetailInfo.dealAmount }}만원</strong>
+                </td>
+              </tr>
+            </table>
           </div>
           <div class="detailMiddleRight">
-            <div class="detailMiddleRightInfo">
-              <v-chip :color="`red lighten-4`" label class="info-item"
-                ><strong>검색주소</strong></v-chip
-              >
-              &nbsp;
-              <strong
-                >{{ sido }} {{ aptDetailInfo.dongName }}
-                {{ aptDetailInfo.roadName }} {{ aptDetailInfo.jibun }}</strong
-              >
-            </div>
-            <div class="detailMiddleRightInfo">
-              <v-chip :color="`red lighten-4`" label class="info-item"
-                ><strong>거래 날짜</strong></v-chip
-              >
-              &nbsp;
-              <strong
-                >{{ aptDetailInfo.dealYear }}년
-                {{ aptDetailInfo.dealMonth }}월</strong
-              >
-            </div>
-            <div class="detailMiddleRightInfo">
-              <v-chip :color="`red lighten-4`" label class="info-item"
-                ><strong>건축 년도</strong></v-chip
-              >
-              &nbsp; <strong>{{ aptDetailInfo.buildYear }}년</strong>
-            </div>
-            <div class="detailMiddleRightInfo">
-              <v-chip :color="`red lighten-4`" label class="info-item">
-                <strong>층</strong></v-chip
-              >
-              &nbsp; <strong>{{ aptDetailInfo.floor }}층</strong>
-            </div>
-            <div class="detailMiddleRightInfo">
-              <v-chip :color="`red lighten-4`" label class="info-item">
-                <strong>거래 금액</strong></v-chip
-              >
-              &nbsp; <strong>{{ aptDetailInfo.dealAmount }}만원</strong>
-            </div>
+            <div id="detail_map" style="width: 700px"></div>
           </div>
         </div>
       </div>
@@ -80,12 +103,60 @@ export default {
   data() {
     return {
       map: Object,
+      markers: [],
     };
   },
   computed: {
     ...mapState(["aptDetailInfo", "sido"]),
   },
-  methods: {},
+  methods: {
+    initMap() {
+      this.markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+
+      const container = document.getElementById("detail_map");
+      const options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 4,
+      };
+      //지도 객체를 등록합니다.
+      //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
+      const map = new kakao.maps.Map(container, options); // 지도를 생성합니다.
+      this.map = map;
+
+      let marker = new kakao.maps.Marker({
+        map: this.map,
+        title: "abc",
+        position: new kakao.maps.LatLng(33.450701, 126.570667),
+      });
+      console.log(marker);
+      // var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+      // // 마커 이미지의 이미지 크기 입니다
+      // var imageSize = new kakao.maps.Size(24, 35);
+
+      // // 마커 이미지를 생성합니다
+      // var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+    },
+  },
+  mounted() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src =
+        "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=fee2964f095ecaef309e6699b5d8e22d";
+      document.head.appendChild(script);
+    }
+  },
+  watch: {
+    aptDetailInfo() {
+      this.map = null;
+      this.initMap();
+    },
+  },
 };
 </script>
 
@@ -100,24 +171,19 @@ export default {
   text-align: center;
 }
 .detailMiddle {
-  width: 30%;
+  display: flex;
   padding: 1rem;
-  float: left;
 }
 .detailMiddleLeft {
   text-align: center;
+  margin: 20px;
+  width: 30%;
 }
 .detailMiddleRight {
-  padding: 1rem;
-}
-.detailMiddleRightInfo {
-  padding: 0.3rem;
-}
-/* .map-section {
-  background-color: bisque;
-  float: left;
+  border: 1px solid black;
+  margin: 20px;
   width: 70%;
-} */
+}
 .info-item {
   width: 80px;
   text-align: center;
